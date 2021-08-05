99fa3c24ad5e05b00d8f0223fe52be8cf8c2db12
Balwant Singh <balwant.singh@deorwine.com>
2021-08-05 15:36:24 +0530

    added env support

--- config/global.js
+++ config/global.js
@@ -1,2 +1,3 @@
 global.MONGO_DB_CONNECTION_STRING =
+  process.env.MONGO_DB_CONNECTION_STRING ||
   "mongodb+srv://dbUser:Mdikr7WAXb0OIlUf@cluster0.nllsy.mongodb.net/test?authSource=admin&replicaSet=atlas-otyxyx-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
