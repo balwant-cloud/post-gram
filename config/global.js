2c1ab3db47f7ad7ec5315c1fc37de3066923440a
Balwant Singh <balwant.singh@deorwine.com>
2021-10-27 18:25:58 +0530

    change env settings

--- config/global.js
+++ config/global.js
@@ -1,3 +1,1 @@
-global.MONGO_DB_CONNECTION_STRING =
-  process.env.MONGO_DB_CONNECTION_STRING ||
-  "mongodb+srv://dbUser:Mdikr7WAXb0OIlUf@cluster0.nllsy.mongodb.net/test?authSource=admin&replicaSet=atlas-otyxyx-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
+global.MONGO_DB_CONNECTION_STRING = process.env.MONGO_DB_CONNECTION_STRING;
